5cb5015d180673710885b74f61b916786074623f
Andrey Ryabin <andreyryabin@yandex.ru>
2016-11-03 21:47:41 +0500

    страница настройки модуля

--- options.php
+++ options.php
@@ -3,8 +3,16 @@ $module_id = "sprint.options";
 
 global $APPLICATION;
 $MODULE_RIGHT = $APPLICATION->GetGroupRight($module_id);
-if (!($MODULE_RIGHT >= "R")){
+if (!($MODULE_RIGHT >= "R")) {
     $APPLICATION->AuthForm("ACCESS_DENIED");
 }
 
 CModule::IncludeModule($module_id);
+
+
+?>
+
+
+<p>Документация к модулю: <br/>
+    <a target="_blank" href="https://bitbucket.org/andrey_ryabin/sprint.options/">https://bitbucket.org/andrey_ryabin/sprint.options/</a>
+</p>

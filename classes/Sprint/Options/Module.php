cf0fe5158579d9f154c3fc592f5211a16d130ae7
Alexandr Nesterov <nesterov@greensight.ru>
2018-12-27 15:21:03 +0300

    add miltiply field

--- classes/Sprint/Options/Module.php
+++ classes/Sprint/Options/Module.php
@@ -9,11 +9,13 @@ class Module
 
     protected static $configCache = array();
 
-    protected static function getDocRoot() {
+    protected static function getDocRoot()
+    {
         return rtrim($_SERVER['DOCUMENT_ROOT'], DIRECTORY_SEPARATOR);
     }
 
-    public static function getPhpInterfaceDir() {
+    public static function getPhpInterfaceDir()
+    {
         if (is_dir(self::getDocRoot() . '/local/php_interface')) {
             return self::getDocRoot() . '/local/php_interface';
         } else {
@@ -21,49 +23,80 @@ class Module
         }
     }
 
-    public static function getModuleDir() {
-        if (is_file(self::getDocRoot() . '/local/modules/'.self::$modulename.'/include.php')) {
+    public static function getModuleDir()
+    {
+        if (is_file(self::getDocRoot() . '/local/modules/' . self::$modulename . '/include.php')) {
             return self::getDocRoot() . '/local/modules/' . self::$modulename;
         } else {
-            return self::getDocRoot() . '/bitrix/modules/'.self::$modulename;
+            return self::getDocRoot() . '/bitrix/modules/' . self::$modulename;
         }
     }
 
-
-    public static function getDbOption($name, $default=''){
+    public static function getDbOption($name, $default = '')
+    {
         $val = \COption::GetOptionString(self::$modulename, $name, null);
-        if (is_null($val)){
+
+        if (is_null($val)) {
             $opts = self::getOptionsConfig();
-            return isset($opts[$name]) ? $opts[$name]['DEFAULT'] : $default;
+            $val = isset($opts[$name]) ? $opts[$name]['DEFAULT'] : $default;
+        }
+
+        if (static::isMulti($name)) {
+            $val = static::getMultiValue($val);
         }
 
         return $val;
     }
 
-    public static function setDbOption($name, $value){
+    public static function isMulti($name)
+    {
+        $opts = self::getOptionsConfig();
+        return $opts[$name]['MULTI'] == 'Y';
+    }
+
+    public static function getMultiValue($value)
+    {
+        if (empty($value)) {
+            return [];
+        }
+
+        return (array) unserialize($value);
+    }
+
+    public static function packValue($value)
+    {
+        $value = array_filter((array) $value);
+        return serialize($value);
+    }
+
+    public static function setDbOption($name, $value)
+    {
+        if (static::isMulti($name)) {
+            $value = static::packValue($value);
+        }
+
         if ($value != \COption::GetOptionString(self::$modulename, $name, '')) {
             \COption::SetOptionString(self::$modulename, $name, $value);
         }
     }
 
-    public static function resetDbOptions(){
+    public static function resetDbOptions()
+    {
         $options = self::getOptionsConfig();
-        foreach ($options as $name => $opt){
+        foreach ($options as $name => $opt) {
             \COption::RemoveOption(self::$modulename, $name);
         }
     }
 
-    public static function getOptionsConfig(){
-        if (empty(self::$configCache)){
+    public static function getOptionsConfig()
+    {
+        if (empty(self::$configCache)) {
             $file = self::getPhpInterfaceDir() . '/sprint.options.php';
-            if (is_file($file)){
-                self::$configCache = include($file);
+            if (is_file($file)) {
+                self::$configCache = include $file;
                 self::$configCache = (self::$configCache && is_array(self::$configCache)) ? self::$configCache : array();
             }
         }
         return self::$configCache;
     }
 }
-
-
-

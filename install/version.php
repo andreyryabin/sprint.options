f2e82ea496fa37787373992e61c0925cea509e89
Andrey Ryabin <andreyryabin@yandex.ru>
2023-03-17 22:26:05 +0500

    2.3.0

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = [
-    "VERSION"      => "2.2.1",
-    "VERSION_DATE" => "2023-03-12 23:40:00",
+    "VERSION"      => "2.3.0",
+    "VERSION_DATE" => "2023-03-12 23:50:00",
 ];
